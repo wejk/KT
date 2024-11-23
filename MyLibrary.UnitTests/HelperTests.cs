73aed19ab450efffdc959eea97b586ce1ee1f2dd
ken <ken@KEN-LAPTOP>
2024-11-23 15:03:22 +1000

    Add project files.

---
+++ MyLibrary.UnitTests/HelperTests.cs
@@ -0,0 +1,115 @@
+﻿namespace MyLibrary.Tests;
+public class HelperTests
+{
+    [Fact]
+    public void ToCamelCase_ShouldConvertToCamelCase()
+    {
+        // Arrange
+        var input = "HelloWorld";
+        var expected = "helloWorld";
+
+        // Act
+        var result = input.ToCamelCase();
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void ToPascalCase_ShouldConvertToPascalCase()
+    {
+        // Arrange
+        var input = "helloWorld";
+        var expected = "HelloWorld";
+
+        // Act
+        var result = input.ToPascalCase();
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void ToSnakeCase_ShouldConvertToSnakeCase()
+    {
+        // Arrange
+        var input = "Hello World";
+        var expected = "hello_world";
+
+        // Act
+        var result = input.ToSnakeCase();
+
+        // Assert
+        Assert.Equal(expected, result);
+    }
+
+    [Fact]
+    public void RandomUniqueWords_WordsIsNull_ThrowsArgumentNullException()
+    {
+        // Arrange
+        IEnumerable<string> words = null;
+        int count = 5;
+
+        // Act & Assert
+        Assert.Throws<ArgumentNullException>(() => StringHelper.RandomUniqueWords(words, count));
+    }
+
+    [Fact]
+    public void ContainsString_ShouldReturnTrueIfStringExists()
+    {
+        // Arrange
+        var searchString = "apple";
+        var stringArray = new[] { "apple", "banana", "cherry" };
+
+        // Act
+        var result = stringArray.ContainsString(searchString);
+
+        // Assert
+        Assert.True(result);
+    }
+
+    [Fact]
+    public void RandomWords_ReturnsCorrectNumberOfWords()
+    {
+        // Arrange
+        var words = new[] { "apple", "banana", "cherry", "date", "elderberry" };
+        int count = 3;
+
+        // Act
+        var result = StringHelper.RandomUniqueWords(words, count);
+
+        // Assert
+        Assert.Equal(count, result.Count());
+    }
+
+    [Fact]
+    public void RandomWords_ReturnsUniqueWords()
+    {
+        // Arrange
+        var words = new[] { "banana", "banana", "banana", "date", "elderberry" };
+        var expected = words.RemoveDuplicateWords().Count();
+        int count = 3;
+
+        // Act
+        var result = StringHelper.RandomUniqueWords(words, count);
+
+        // Assert
+        Assert.Equal(expected, result.Count());
+    }
+
+    [Fact]
+    public void RandomWords_ReturnsAllWordsIfCountIsGreaterThanUniqueArrayLength()
+    {
+        // Arrange
+        var words = new[] { "banana", "banana", "cherry", "date", "elderberry" };
+        var expected = words.RemoveDuplicateWords().Count();
+        int count = 5;
+
+        // Act
+        var result = StringHelper.RandomUniqueWords(words, count);
+
+        // Assert
+        Assert.Equal(expected, result.Count());
+        Assert.All(words, word => Assert.Contains(word, result));
+    }
+}
---
+++ MyLibrary/StringHelper.cs
@@ -0,0 +1,81 @@
+﻿using System.Buffers;
+
+namespace MyLibrary
+{
+    public static class StringHelper
+    {
+        public static string ToCamelCase(this string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return str;
+            }
+            return char.ToLower(str[0]) + str.Substring(1);
+        }
+
+        public static string ToPascalCase(this string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return str;
+            }
+            return char.ToUpper(str[0]) + str.Substring(1);
+        }
+
+        public static string ToSnakeCase(this string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return str;
+            }
+            return string.Join("_", str.Split(' ')).ToLower();
+        }
+
+        public static bool ContainsString(this string[] stringArray, string searchItem)
+        {
+            return stringArray.Contains(searchItem);
+        }
+
+        public static int WordCount(this string str)
+        {
+            return str.Split(" ", StringSplitOptions.RemoveEmptyEntries).Length;
+        }
+
+        /// <summary>
+        /// Returns a random set of unique words from the input list of words.
+        /// </summary>
+        /// <param name="words">When words is null</param>
+        /// <param name="numberOfWords"></param>
+        /// <exception cref="ArgumentNullException">words is null</exception>"
+        /// <returns></returns>
+        public static IEnumerable<string> RandomUniqueWords(IEnumerable<string> words, int numberOfWords)
+        {
+            var uniqueWords = words.RemoveDuplicateWords();
+            var maxCount = uniqueWords.Count();
+
+            if (numberOfWords > maxCount)
+            {
+                return uniqueWords;
+            }
+
+            var temp = new string[numberOfWords];
+            var rand = new Random();
+
+            for (var i = 0; i < numberOfWords; i++)
+            {
+                var randomIndex = rand.Next(maxCount);
+                temp[i] = words.ElementAt(randomIndex);
+            }
+            return temp;
+        }
+
+        public static IEnumerable<string> RemoveDuplicateWords(this IEnumerable<string> words)
+        {
+            if (!words.Any())
+            {
+                return words;
+            }
+            return words.Select(x => x).Distinct();
+        }
+    }
+}
